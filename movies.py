c19ffd717251a13f2e04d15b53c80529cb05debf
rozqlinq <alkeyeva.rozalina@gmail.com>
2024-06-28 14:49:34 +0500

    Update movies.py

--- movies.py
+++ movies.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import seaborn as sns
+import pickle
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
